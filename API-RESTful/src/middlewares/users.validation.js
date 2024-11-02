c82e8a01848ee9a0c8cd48c1bd4f2c92ee8ab050
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-01 22:12:15 -0500

    Sprint 1
    Validación de campos en el registro

--- API-RESTful/src/middlewares/users.validation.js
+++ API-RESTful/src/middlewares/users.validation.js
@@ -1,3 +1,38 @@
 import { check, validationResult } from "express-validator";
 
-export const validateUser = [];
+export const validateUser = [
+    check(["name", "lastname", "mail", "password"])
+        .exists()
+        .withMessage("Los campos nombre, apellido, correo y contraseña son oligatorios")
+        .notEmpty()
+        .withMessage("Los campos nombre, apellido, correo y/o contraseña no pueden estar vacíos")
+        .customSanitizer(value => value?.trim()),
+
+    check(["name", "lastname"])
+        .isString()
+        .isLength({min: 3, max: 20})
+        .isAlpha("es-ES", {ignore: "áéíóúñÁÉÍÓÚÑ"})
+        .withMessage("El nombre y apellido deben contener solo letras")
+        .customSanitizer(value => value?.trim()),
+
+
+    check("mail")
+        .isEmail()
+        .withMessage("El correo electrónico no es válido")
+        .customSanitizer(value => value?.trim()),
+
+    check("password")
+        .isLength({min: 8})
+        .withMessage("La contraseña debe tener al menos 8 caracteres")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).*$/)
+        .withMessage("La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un caracter especial")
+        .customSanitizer(value => value?.trim()),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).send({errors : errors.array()})
+        }
+        return next();
+    }
+];
--- API-RESTful/src/routers/users.routes.js
+++ API-RESTful/src/routers/users.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import {
     userRegister
 } from "../controllers/users.controller.js";
+import { validateUser } from "../middlewares/users.validation.js";
 
 const router = Router();
 
 
-router.post('/sign-up', userRegister);
+router.post('/sign-up',validateUser, userRegister);
 
 export default router;
