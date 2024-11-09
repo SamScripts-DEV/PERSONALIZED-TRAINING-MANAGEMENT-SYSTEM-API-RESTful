08a15e8a8020a557d4e9ceafbff6a7880932a07b
Dustin1904 <dustin04ms@gmail.com>
2024-11-09 16:11:25 -0500

    cors

--- API-RESTful/src/index.js
+++ API-RESTful/src/index.js
@@ -3,7 +3,7 @@ import connection from './database.js';
 
 connection();
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
 });
      
--- API-RESTful/src/server.js
+++ API-RESTful/src/server.js
@@ -12,7 +12,11 @@ const app = express();
 
 //Configuramos el puerto
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 
 app.use(express.json()); 
