11a182768ab8a6a6951ef05f552e0583a2d8b0b5
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-12 22:48:37 -0500

    Actualizacion
    Ruta cliente

--- API-RESTful/src/services/exercisesAPI.js
+++ API-RESTful/src/services/exercisesAPI.js
@@ -19,6 +19,30 @@ const fetchallexercises = async() => {
     }
 };
 
+const fetchExcercisesByID = async(id) => {
+    
+        const options = {
+            method: 'GET',
+            url: `https://exercise-db-fitness-workout-gym.p.rapidapi.com/exercises/${id}`,
+            headers: {
+                'x-rapidapi-key': process.env.API_KEY,
+                'x-rapidapi-host': process.env.API_HOST
+            }
+        }
+    
+        try {
+            const response = await axios.request(options);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+};
+
+const getEsercisesWithDetails = async() => {
+    
+}
+
+
 
 const fetchTypesforExercises = async() => {
     
