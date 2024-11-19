7413c832d6de19521c1ab3eafa5e872a94d40332
Samuel Cuti <robsamuel.cuti@epn.edu.ec>
2024-11-19 10:16:06 -0500

    Actualización
    Permitir ver Coach a los clientes

--- API-RESTful/src/routers/coach.routes.js
+++ API-RESTful/src/routers/coach.routes.js
@@ -20,8 +20,8 @@ const router = Router();
 router.post('/coach/register', coachRegister);  //Registra un entrenador
 
 //Private routes
-router.get('/coach/view-coaches',verifyAuth,verifyAdminRole, viewCoaches); //Muestra todos los entrenadores
-router.get('/coach/view-coach/:id',verifyAuth,verifyAdminRole, viewCoachById); //Muestra un entrenador por id
+router.get('/coach/view-coaches',verifyAuth, viewCoaches); //Muestra todos los entrenadores
+router.get('/coach/view-coach/:id',verifyAuth, viewCoachById); //Muestra un entrenador por id
 router.put('/coach/update-coach/:id',verifyAuth, updateCoach); //Actualiza un entrenador por id
 router.delete('/coach/delete-coach/:id',verifyAuth,verifyAdminRole, deleteCoach); //Elimina un entrenador por id
 
