586a63da46fc2cfc03d73c54b91cc3545f13b8cb
JohnMata0427 <jhonmata0427@gmail.com>
2024-12-28 11:24:16 -0500

    Corrección para la obtenciónd de días completados del cliente

--- API-RESTful/src/controllers/completed_days.controller.js
+++ API-RESTful/src/controllers/completed_days.controller.js
@@ -3,22 +3,26 @@ import CompletedDays from '../models/completedDays.js';
 
 export const markDaysAsCompleted = async (req, res) => {
     try {
-        const { userBDD: { _id: user_id }, body: { day }} = req;
+        const {
+            userBDD: { _id: user_id },
+            body: { day },
+        } = req;
 
-        const quitarAcentos = (cadena) => cadena.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        const quitarAcentos = (cadena) =>
+            cadena.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
         const currentDay = quitarAcentos(
             new Date().toLocaleString('es-ES', { weekday: 'long' }),
         );
 
-        if (day !== currentDay) 
+        if (day !== currentDay)
             return res
                 .status(400)
                 .json({ res: 'El día no coincide con el día actual' });
 
         const client_id = await Client.exists({ user_id });
 
-        if (!client_id) 
+        if (!client_id)
             return res.status(404).json({ res: 'Cliente no encontrado' });
 
         const completeDay = await CompletedDays.findOneAndUpdate(
@@ -40,12 +44,12 @@ export const viewCompletedDays = async (req, res) => {
     try {
         const { _id: user_id } = req.userBDD;
 
-        if (!await Client.exists({ user_id })) 
+        if (!(await Client.exists({ user_id })))
             return res.status(404).json({ res: 'Cliente no encontrado' });
 
         const completedDays = await CompletedDays.find({
             user_id,
-        }).sort({ date: -1 });
+        });
 
         res.status(200).json({
             res: 'Días completados encontrados',
